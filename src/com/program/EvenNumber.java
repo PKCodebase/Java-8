8c35e1c74b185cdc1a74ede0d20fc3dd61bb9ca3
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-09 16:18:13 +0530

    Even and Odd

---
+++ src/com/program/EvenNumber.java
@@ -0,0 +1,23 @@
+package com.program;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenNumber {
+    public static void main(String[] args){
+        List<Integer> num = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        num
+                .stream()
+                .filter(n->n%2==0).
+                forEach(n->System.out.print(n+" "));
+
+        
+        System.out.println();
+        System.out.println("-----------");
+
+        num
+                .stream()
+                .filter(n->n%2!=0)
+                .forEach(n->System.out.print(n+" "));
+    }
+}
