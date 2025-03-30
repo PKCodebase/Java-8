d041b05ad60526026e74a0746d041db7332ae568
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-30 16:26:41 +0530

    CountCharacter

---
+++ src/com/program/CountCharacter.java
@@ -0,0 +1,9 @@
+package com.program;
+
+import java.util.List;
+
+public class CountCharacter {
+    public static void main(String[]args){
+        List<String> mylist = List.of("kaushik","prasad");
+    }
+}
