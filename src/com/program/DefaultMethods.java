f49cceef14903c079ce2436a8aac342a0016da1a
PKCodebase <kaushikprasad1659@gmail.com>
2025-03-30 16:36:11 +0530

    DefaultMethods

---
+++ src/com/program/DefaultMethods.java
@@ -0,0 +1,40 @@
+package com.program;
+
+interface A{
+    default void display(){
+        System.out.println("A");
+    }
+}
+
+
+//interface B{
+//    default void display(){
+//        System.out.println("B");
+//    }
+//}
+
+
+//
+//class C implements A,B{
+//    public void display(){
+//        B.super.display();
+//        A.super.display();
+//        System.out.println("C");
+//
+//    }
+//}
+
+
+//public class DefaultMethods extends C {
+//    public static void main (String[]args){
+//        DefaultMethods d = new DefaultMethods();
+//        d.display();
+//    }
+//}
+
+public class DefaultMethods implements A {
+    public static void main (String[]args){
+        DefaultMethods d = new DefaultMethods();
+        d.display();
+    }
+}
