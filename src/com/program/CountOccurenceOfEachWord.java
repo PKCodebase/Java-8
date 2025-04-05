70e535c9afeea94d6c424ce9b59b2f4bcd7740b0
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-05 16:31:53 +0530

    CountOccurenceOfEachWord

---
+++ src/com/program/CountOccurenceOfEachWord.java
@@ -0,0 +1,19 @@
+package com.program;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountOccurenceOfEachWord {
+    public static void main(String[] args){
+
+        String str = "I am a good boy";
+        Map<String,Long> wordCount = Arrays.stream(str.split(" "))
+
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+
+        System.out.println(wordCount);
+    }
+}
